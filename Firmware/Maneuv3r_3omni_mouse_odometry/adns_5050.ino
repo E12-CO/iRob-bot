e9b79a2b61588f7bba8b52e3bb0e5d19ae4f1421
TiNredmc <thipok17@gmail.com>
2024-07-20 20:05:30 +0700

    SPI is now working properly. TODO: attach LED under the robot chassis.
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- Firmware/Maneuv3r_3omni_mouse_odometry/adns_5050.ino
+++ Firmware/Maneuv3r_3omni_mouse_odometry/adns_5050.ino
@@ -1,30 +1,40 @@
+// Optical flow sensor (mouse sensor)
 #define REG_PRODID  0x00 // Use to verify the Serial communication
 #define REG_MOTION  0x02 // Motion detection register
 #define REG_DELTAX  0x03 // Delta X movement 
 #define REG_DELTAY  0x04 // Delta Y movement
+#define REG_SQUAL   0x05 // Surface quality
 #define REG_GETPIX  0x0B // Get Raw pixel reading
 #define REG_MCTRL2  0x10 // Mouse control 2 (setting DPI)
 #define REG_RDBRST  0x63 // Motion Burst, Read Delta X and Y in single command
 #define REG_LEDCTR  0x22 // LED control (strobe mode or always on)
 
 typedef struct pin_t{
-  int ncs;
-  int sio;
-  int sck;
+  uint8_t ncs;
+  uint8_t sio;
+  uint8_t sck;
+  uint8_t nrst;
 };
 
 pin_t adns_Pin_t;
 
-void adns5050_init(int ncs, int sio, int sck) {
+void adns5050_init(int ncs, int sio, int sck, int nrst) {
   // Setup GPIO
   adns_Pin_t.ncs = ncs;
   adns_Pin_t.sio = sio;
   adns_Pin_t.sck = sck;
+  adns_Pin_t.nrst = nrst;
 
   pinMode(adns_Pin_t.ncs, OUTPUT);
   digitalWrite(adns_Pin_t.ncs, HIGH);
   pinMode(adns_Pin_t.sio, INPUT);
   pinMode(adns_Pin_t.sck, OUTPUT);
+  digitalWrite(adns_Pin_t.sck, HIGH);
+  
+  pinMode(adns_Pin_t.nrst, OUTPUT);
+  digitalWrite(adns_Pin_t.nrst, LOW);
+  delay(50);
+  digitalWrite(adns_Pin_t.nrst, HIGH);
 }
 
 // Bit-banging register 8 bit read
@@ -34,23 +44,24 @@ uint8_t adns5050_readReg(uint8_t reg) {
   digitalWrite(adns_Pin_t.ncs, LOW);
   pinMode(adns_Pin_t.sio, OUTPUT);
   for (uint8_t i = 0; i < 8; i ++) {
-    digitalWrite(adns_Pin_t.sio, (reg && 0x80) ? HIGH : LOW);
     digitalWrite(adns_Pin_t.sck, HIGH);
+    digitalWrite(adns_Pin_t.sio, (reg & 0x80) ? HIGH : LOW);
     digitalWrite(adns_Pin_t.sck, LOW);
-    reg <<= 1;
+    reg = reg << 1;
   }
-  pinMode(adns_Pin_t.sio, INPUT);
+  
   delayMicroseconds(4);
+  digitalWrite(adns_Pin_t.sio, LOW);
+  pinMode(adns_Pin_t.sio, INPUT);
 
   for (uint8_t i = 0; i < 8; i++) {
-    digitalWrite(adns_Pin_t.sck, HIGH);
-    ret |= digitalRead(adns_Pin_t.sio);
     digitalWrite(adns_Pin_t.sck, LOW);
-    ret <<= 1;
+    ret |= digitalRead(adns_Pin_t.sio);
+    digitalWrite(adns_Pin_t.sck, HIGH);
+    ret = ret << 1;
   }
 
   digitalWrite(adns_Pin_t.ncs, HIGH);
-
   return ret;
 }
 
@@ -61,24 +72,25 @@ void adns5050_writeReg(uint8_t reg, uint8_t val) {
   digitalWrite(adns_Pin_t.ncs, LOW);
   pinMode(adns_Pin_t.sio, OUTPUT);
   for (uint8_t i = 0; i < 8; i ++) {
-    digitalWrite(adns_Pin_t.sio, (reg && 0x80) ? HIGH : LOW);
-    digitalWrite(adns_Pin_t.sck, HIGH);
     digitalWrite(adns_Pin_t.sck, LOW);
-    reg <<= 1;
+    digitalWrite(adns_Pin_t.sio, (reg & 0x80) ? HIGH : LOW);
+    digitalWrite(adns_Pin_t.sck, HIGH);
+    reg = reg << 1;
   }
   for (uint8_t i = 0; i < 8; i++) {
-    digitalWrite(adns_Pin_t.sio, (val && 0x80) ? HIGH : LOW);
-    digitalWrite(adns_Pin_t.sck, HIGH);
     digitalWrite(adns_Pin_t.sck, LOW);
-    val <<= 1;
+    digitalWrite(adns_Pin_t.sio, (val & 0x80) ? HIGH : LOW);
+    digitalWrite(adns_Pin_t.sck, HIGH);
+    val = val << 1;
   }
+  
   pinMode(adns_Pin_t.sio, INPUT);
   digitalWrite(adns_Pin_t.ncs, HIGH);
 }
 
 // Probe for sensor by reading the sensor ID
 uint8_t adns5050_Probe() {
-  return adns5050_readReg(REG_PRODID) != 0x12;
+  return (adns5050_readReg(REG_PRODID) != 0x12);
 }
 
 // Turn Lighting LED on 
@@ -86,47 +98,53 @@ void adns5050_setLED() {
   adns5050_writeReg(REG_LEDCTR, 0x80);
 }
 
-// Set Count Per Inch to 625 Count per Inch
-void adns5050_set625cpi(){
+// Set Count Per Inch to 1375 Count per Inch
+void adns5050_setcpi(){
   adns5050_writeReg(REG_MCTRL2, 0x10);// First, enable the RES register
-  adns5050_writeReg(REG_MCTRL2, 0x15);// Select 625CPI
+  adns5050_writeReg(REG_MCTRL2, 0x1B);// Select 1375CPI
+}
+
+uint8_t adns5050_getSurfaceQ(){
+  return adns5050_readReg(REG_SQUAL);
 }
 
 // Read the travel distance of X and Y axis
 // The readings are in Count per Inch unit,
 // Inch to meter conversion will be perform separately 
 void adns5050_getdXdY(int8_t *dX, int8_t *dY) {
-  uint8_t reg, ret = 0;
+  uint8_t reg = 0;
 
   reg = REG_RDBRST;
   
   digitalWrite(adns_Pin_t.ncs, LOW);
   pinMode(adns_Pin_t.sio, OUTPUT);
   for (uint8_t i = 0; i < 8; i ++) {
-    digitalWrite(adns_Pin_t.sio, (reg && 0x80) ? HIGH : LOW);
-    digitalWrite(adns_Pin_t.sck, HIGH);
     digitalWrite(adns_Pin_t.sck, LOW);
-    reg <<= 1;
+    digitalWrite(adns_Pin_t.sio, (reg & 0x80) ? HIGH : LOW);
+    digitalWrite(adns_Pin_t.sck, HIGH);
+    reg = reg << 1;
   }
-  pinMode(adns_Pin_t.sio, INPUT);
+  
   delayMicroseconds(4);
+  digitalWrite(adns_Pin_t.sio, LOW);
+  pinMode(adns_Pin_t.sio, INPUT);
 
   for (uint8_t i = 0; i < 8; i++) {
-    digitalWrite(adns_Pin_t.sck, HIGH);
-    ret |= digitalRead(adns_Pin_t.sio);
     digitalWrite(adns_Pin_t.sck, LOW);
-    ret <<= 1;
+    reg |= digitalRead(adns_Pin_t.sio);
+    digitalWrite(adns_Pin_t.sck, HIGH);
+    reg = reg << 1;
   }
-  *dX = ret;
+  *dX = (int8_t)reg;
 
   for (uint8_t i = 0; i < 8; i++) {
-    digitalWrite(adns_Pin_t.sck, HIGH);
-    ret |= digitalRead(adns_Pin_t.sio);
     digitalWrite(adns_Pin_t.sck, LOW);
-    ret <<= 1;
+    reg |= digitalRead(adns_Pin_t.sio);
+    digitalWrite(adns_Pin_t.sck, HIGH);
+    reg = reg << 1;
   }
-  *dY = ret;
-
+  *dY = (int8_t)reg;
+  
   digitalWrite(adns_Pin_t.ncs, HIGH);
 }
 
@@ -134,9 +152,9 @@ int8_t dx, dy;
 void adns5050_updateVel(odometry_t *robot_odom){
     adns5050_getdXdY(&dx, &dy);
 
-    // At 1000 CPI, still need calibration tho, as I will use different optics assembly.
-    //vx = (vx/625) * 125 * 0.0254;// CPI->Inch->InchPerSec->MeterPerSec
-    //vy = (vy/625) * 125 * 0.0254;// CPI->Inch->InchPerSec->MeterPerSec
+    // At 1375 CPI, still need calibration tho, as I will use different optics assembly.
+    //vx = (vx/1375) * 125 * 0.0254;// CPI->Inch->InchPerSec->MeterPerSec
+    //vy = (vy/1375) * 125 * 0.0254;// CPI->Inch->InchPerSec->MeterPerSec
     
     robot_odom->vel_x = dx * 0.003175;
     robot_odom->vel_y = dy * 0.003175;
