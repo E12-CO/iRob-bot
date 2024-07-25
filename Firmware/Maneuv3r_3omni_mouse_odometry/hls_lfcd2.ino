7d1090a367a0c81dc35a09cb273f04de6630df4e
TiNredmc <thipok17@gmail.com>
2024-07-25 09:02:59 +0700

    [W.I.P]Using packed struct for 8 bit access.
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- Firmware/Maneuv3r_3omni_mouse_odometry/hls_lfcd2.ino
+++ Firmware/Maneuv3r_3omni_mouse_odometry/hls_lfcd2.ino
@@ -1,15 +1,21 @@
 #define TX2_pin 17
 #define RX2_pin 16
 
-typedef struct lidar_data {
-  //uint8_t sync_byte;
-  uint16_t angle_index;
+typedef struct __attribute__ ((packed)) lidar_data{
+  union{
+    uint16_t header;
+    struct{
+      uint8_t sync_byte :8;
+      uint8_t angle_idx :8;
+    };
+  };
   uint16_t motor_rpm;
   struct {
     uint16_t intensity;
     uint16_t distance;
     uint16_t reserved;
   } measurement[6];
+  uint16_t checksum;
 };
 
 lidar_data hls_data;
@@ -17,29 +23,33 @@ lidar_data hls_data;
 void hls_initLiDAR() {
   Serial2.begin(230400, SERIAL_8N1, RX2_pin, TX2_pin);
   Serial2.write('e');
-  delay(100);
-  Serial2.write('b');
 }
 
 uint8_t hls_fsm;
 uint8_t hls_idx;
 void hls_poll(){
   switch (hls_fsm) {
-    case 0:// Detect frame start
+    case 0:
+      {
+        Serial2.write('b');
+        hls_fsm = 1;
+      }
+    case 1:// Detect frame start
       {
         if (0xFA == Serial2.read()) {
-          hls_fsm = 1;
+          hls_idx++;
+          hls_fsm = 2;
         }
       }
       break;
 
-    case 1:
+    case 2:
       {
         *(uint8_t *)(&hls_data + hls_idx) = Serial2.read();
         hls_idx++;
         if(hls_idx > 41){
           hls_idx = 0;
-          hls_fsm = 0;
+          hls_fsm = 1;
         }
       }
       break;
