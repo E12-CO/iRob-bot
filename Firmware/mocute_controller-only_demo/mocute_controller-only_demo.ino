8ae3a4e36c145b6b26b2c5b2470f4532d41023e3
TiNredmc <thipok17@gmail.com>
2024-07-30 20:29:48 +0700

    Example code of using Bluepad32 with ESP32.
    Signed-off-by: TiNredmc <thipok17@gmail.com>

---
+++ Firmware/mocute_controller-only_demo/mocute_controller-only_demo.ino
@@ -0,0 +1,101 @@
+#include <Bluepad32.h>
+#include <uni.h>
+
+ControllerPtr myControllers;
+
+// This callback gets called any time a new gamepad is connected.
+// Up to 4 gamepads can be connected at the same time.
+void onConnectedController(ControllerPtr ctl) {
+    bool foundEmptySlot = false;
+        if (myControllers == nullptr) {
+            Serial.printf("CALLBACK: Controller is connected");
+            // Additionally, you can get certain gamepad properties like:
+            // Model, VID, PID, BTAddr, flags, etc.
+            ControllerProperties properties = ctl->getProperties();
+            Serial.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id,
+                           properties.product_id);
+            myControllers = ctl;
+            foundEmptySlot = true;
+            return;
+        }
+    if (!foundEmptySlot) {
+        Serial.println("CALLBACK: Controller connected, but could not found empty slot");
+    }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+    bool foundController = false;
+
+        if (myControllers == ctl) {
+            Serial.printf("CALLBACK: Controller disconnected");
+            myControllers = nullptr;
+            foundController = true;
+            return;
+        }
+
+    if (!foundController) {
+        Serial.println("CALLBACK: Controller disconnected, but not found in myControllers");
+    }
+}
+
+void processGamepad(ControllerPtr ctl) {
+   Serial.printf(
+        "dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
+        "misc: 0x%02x\n",
+        ctl->dpad(),         // D-pad
+        ctl->buttons(),      // bitmask of pressed buttons
+        ctl->axisX(),        // (-511 - 512) left X Axis
+        ctl->axisY(),        // (-511 - 512) left Y axis
+        ctl->axisRX(),       // (-511 - 512) right X axis
+        ctl->axisRY(),       // (-511 - 512) right Y axis
+        ctl->brake(),        // (0 - 1023): brake button
+        ctl->throttle(),     // (0 - 1023): throttle (AKA gas) button
+        ctl->miscButtons()  // bitmask of pressed "misc" buttons
+    );
+}
+
+void processControllers() {
+        if (myControllers && myControllers->isConnected() && myControllers->hasData()) {
+            if (myControllers->isGamepad()) {
+                processGamepad(myControllers);
+            } else {
+                Serial.println("Unsupported controller");
+            }
+        }
+}
+
+// Arduino setup function. Runs in CPU 1
+void setup() {
+    bd_addr_t controller_addr = {0xE0, 0xF8, 0x48, 0x05, 0x00, 0x61};
+  
+    Serial.begin(115200);
+    Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+    const uint8_t* addr = BP32.localBdAddress();
+    Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+    // Setup the Bluepad32 callbacks
+    BP32.setup(&onConnectedController, &onDisconnectedController);
+    BP32.forgetBluetoothKeys();
+    BP32.enableVirtualDevice(false);
+    
+    uni_bt_allowlist_add_addr(controller_addr);
+    uni_bt_allowlist_set_enabled(true);
+}
+
+// Arduino loop function. Runs in CPU 1.
+void loop() {
+    // This call fetches all the controllers' data.
+    // Call this function in your main loop.
+    bool dataUpdated = BP32.update();
+    if (dataUpdated)
+        processControllers();
+
+    // The main loop must have some kind of "yield to lower priority task" event.
+    // Otherwise, the watchdog will get triggered.
+    // If your main loop doesn't have one, just add a simple `vTaskDelay(1)`.
+    // Detailed info here:
+    // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
+
+    //     vTaskDelay(1);
+    delay(10);
+}
